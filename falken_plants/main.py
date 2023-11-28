8cd1d11cb67f4f341c39381d4f2e2311ee0e3a92
Richi <r.rodriguez@bbva.com>
2023-11-28 07:53:43 +0100

    Preparint API plants!

--- falken_plants/main.py
+++ falken_plants/main.py
@@ -3,6 +3,7 @@
 
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+from datetime import date
 
 from .logger import Log
 
@@ -46,6 +47,8 @@ def calendar():
     return render_template('calendar.html')
 
 
+###### API plants ######
+
 @main.route("/create_plant", methods=['GET'])
 @login_required
 def create_plant():
@@ -55,24 +58,100 @@ def create_plant():
     return render_template('create_plant.html')
 
 
-
-
-
-
-
 @main.route("/plants", methods=['GET'])
 @login_required
-def add_plant():
-    Log.info("Add plant page")
+def view_all_plants():
+    Log.info("View all the plants page")
     Log.debug(f"Current user: {current_user}")
 
     # return render_template('add_plant.html')
 
 
-@main.route("/plants/<int:plant_id>", methods=['GET', 'POST', 'DELETE'])
+@main.route("/plants/<int:plant_id>", methods=['GET'])
 @login_required
-def edit_plant(plant_id: int):
-    Log.info("Edit plant page")
+def view_plant(plant_id: int):
+    Log.info("View plant page")
+    Log.debug(f"Current user: {current_user}")
+
+    pass
+
+
+@main.route("/plants", methods=['POST'])
+@login_required
+def add_plant():
+    Log.info("Add plant page")
+    Log.debug(f"Current user: {current_user}")
+
+    pass
+
+
+@main.route("/plants/<int:plant_id>", methods=['PUT'])
+@login_required
+def update_plant(plant_id: int):
+    Log.info("Update plant page")
+    Log.debug(f"Current user: {current_user}")
+
+    pass
+
+
+@main.route("/plants/<int:plant_id>", methods=['DELETE'])
+@login_required
+def delete_plant(plant_id: int):
+    Log.info("Delete plant page")
+    Log.debug(f"Current user: {current_user}")
+
+    pass
+
+
+@main.route("/plants/<int:plant_id>/calendar/water", methods=['POST'])
+@login_required
+def water_plant(plant_id: int):
+    Log.info("Water plant page")
+    Log.debug(f"Current user: {current_user}")
+
+    pass
+
+
+@main.route("/plants/<int:plant_id>/calendar/fertilize", methods=['POST'])
+@login_required
+def fertilize_plant(plant_id: int):
+    Log.info("Fertilize plant page")
+    Log.debug(f"Current user: {current_user}")
+
+    pass
+
+
+@main.route("/plants/<int:plant_id>/calendar", methods=['GET'])
+@login_required
+def view_calendar(plant_id: int):
+    Log.info("View calendar page")
+    Log.debug(f"Current user: {current_user}")
+
+    pass
+
+
+@main.route("/plants/<int:plant_id>/calendar/<date>", methods=['GET'])
+@login_required
+def view_calendar_date(plant_id: int, date: date):
+    Log.info("View calendar date page")
+    Log.debug(f"Current user: {current_user}")
+
+    pass
+
+
+@main.route("/plants/<int:plant_id>/calendar/<date>", methods=['POST'])
+@login_required
+def add_calendar_date(plant_id: int, date: date):
+    Log.info("Add calendar date page")
+    Log.debug(f"Current user: {current_user}")
+
+    pass
+
+
+@main.route("/plants/<int:plant_id>/calendar/<date>", methods=['PUT'])
+@login_required
+def update_calendar_date(plant_id: int, date: date):
+    Log.info("Update calendar date page")
     Log.debug(f"Current user: {current_user}")
 
     pass
