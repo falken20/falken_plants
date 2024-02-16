de5fbcc02847397000eb95925b78fffc3674edb1
Richi <r.rodriguez@bbva.com>
2024-02-16 08:17:27 +0100

    Adding config params for best practices!

--- falken_plants/config.py
+++ falken_plants/config.py
@@ -3,6 +3,7 @@
 
 # Library that uses type annotation to validate data and manage settings.
 # from pydantic import BaseSettings # Old version
+import os
 from pydantic.v1 import BaseSettings
 # from pydantic_settings import BaseSettings # New version
 
@@ -65,3 +66,42 @@ def print_settings(settings: Settings) -> None:
             \n env_name: {settings.env_name}\
             \n ENV_PRO: {settings.ENV_PRO}\
             \n LEVEL_LOG: {settings.LEVEL_LOG}")
+
+
+#######################################################################
+# Config format for Flask apps, you create a class for each environment
+#######################################################################
+
+
+class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv("DEVELOPMENT_DATABASE_URL")
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv("TEST_DATABASE_URL")
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv("STAGING_DATABASE_URL")
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.getenv("PRODUCTION_DATABASE_URL")
+
+
+config = {
+    "development": DevelopmentConfig,
+    "testing": TestingConfig,
+    "staging": StagingConfig,
+    "production": ProductionConfig
+}
