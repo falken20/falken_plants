3f6976e1618164a3618009b09fd499897528473f
Richi <r.rodriguez@bbva.com>
2024-05-20 07:57:23 +0200

    Label splash visible control

--- static/js/falken.js
+++ static/js/falken.js
@@ -1,3 +1,4 @@
+// Toggle visibility of elements
 function toggleVisibility(id) {
     var el = document.getElementById(id);
  
@@ -9,11 +10,11 @@ function toggleVisibility(id) {
     }
 }
 
+// Active visibility of elements based on checkbox
 function activeVisibility(id_status, id) {
     var elStatus = document.getElementById(id_status);
     var el = document.getElementById(id);
-
-    console.log(elStatus.checked);
+    console.log("This is a tests", elStatus.checked);
  
     if (elStatus.checked) {
            el.style.visibility="visible";
