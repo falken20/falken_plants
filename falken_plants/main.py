6b729cdfb058d98bce19934b1ccdde1bad3962b4
Richi <r.rodriguez@bbva.com>
2024-10-12 23:23:42 +0200

    Fix the update API method

--- falken_plants/main.py
+++ falken_plants/main.py
@@ -65,35 +65,36 @@ def calendar():
 
 # ###### API plants ######
 
-@main.route("/plants/create", methods=['GET'])
-@login_required
-def create_plant():
-    Log.info("Create plant page")
-    Log.debug(f"Current user: {current_user}")
-
-    return render_template('plant_form.html', plant=None)
-
-
-# TODO: Finish this
-@main.route("/plants/update/<int:plant_id>", methods=['GET'])
-@login_required
-def update_plant(plant_id: int):
-    Log.info("Update plant page")
-    Log.debug(f"Current user: {current_user}")
-
-    plant = ControllerPlant.get_plant(plant_id)
-    Log.debug(f"Plant to update: {plant}")
-
-    return render_template('plant_form.html', plant=plant)
-
-
 @main.route("/plants", methods=['GET'])
 @login_required
 def view_all_plants():
     Log.info("View all the plants page")
     Log.debug(f"Current user: {current_user}")
 
-    # return render_template('add_plant.html')
+    all_plants = ControllerPlant.get_all_plants(current_user.id)
+
+    return render_template('plant_list.html', plants=all_plants, message="")
+
+
+@main.route("/plants/create", methods=['GET'])
+@login_required
+def create_plant():
+    Log.info("Create plant page")
+    Log.debug(f"Current user: {current_user}")
+
+    return render_template('plant_form.html', plant=None, form_method="POST")
+
+
+@main.route("/plants/update/<int:plant_id>", methods=['GET'])
+@login_required
+def update_plant(plant_id: int):
+    Log.info("Edit plant page")
+    Log.debug(f"Current user: {current_user}")
+
+    plant = ControllerPlant.get_plant(plant_id)
+    Log.debug(f"Plant to update: {plant}")
+
+    return render_template('plant_form.html', plant=plant, form_method="PUT")
 
 
 @main.route("/plants/<int:plant_id>", methods=['GET'])
@@ -130,13 +131,32 @@ def post_plant():
     return render_template('plant_list.html')
 
 
+@main.route("/plants/", methods=['PUT'])
 @main.route("/plants/<int:plant_id>", methods=['PUT'])
 @login_required
 def put_plant(plant_id: int):
     Log.info("Update plant page")
     Log.debug(f"Current user: {current_user}")
 
-    pass
+    try:
+        Log.debug(f"Request form: {request.form}")
+        plant_id = plant_id if plant_id else request.form['plant_id']
+        plant = ControllerPlant.update_plant(plant_id=plant_id,
+                                             name=request.form['name'],
+                                             name_tech=request.form['name_tech'],
+                                             comment=request.form['comment'],
+                                             watering_summer=request.form['watering_summer'],
+                                             watering_winter=request.form['watering_winter'],
+                                             spray=request.form['spray'],
+                                             direct_sun=request.form['direct_sun'],
+                                             image=request.form['image'],
+                                             user_id=current_user.id)
+        Log.info(f"Plant updated: {plant}")
+    except Exception as e:
+        Log.error("Error updating plant", err=e, sys=sys)
+        return render_template('plant_form.html', error=e)
+
+    return render_template('plant_list.html')
 
 
 # TODO: @main.route("/plants/<int:plant_id>", methods=['DELETE'])
